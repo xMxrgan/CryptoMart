9c2e6556ee2aedd8becbe2883815803cf351c2ac
Morgan <104979421+xMxrgan@users.noreply.github.com>
2025-04-16 10:10:03 +0200

    Connected mySql database

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get("/", function() {
+    return view ('welcome');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
