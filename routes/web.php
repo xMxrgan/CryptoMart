677b526fd8ef60e046bf00d0dc0d77f4d9da2a4d
Simone Franco <simone.franco@ipsumlab.it>
2025-04-30 17:50:56 +0200

    update route apiResources with prefix api

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,19 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-Route::apiResources([
-    "products" => \App\Http\Controllers\ProductController::class,
-]);
+Route::prefix('api')->group(function () {
+    Route::apiResources([
+        "products" => \App\Http\Controllers\ProductController::class,
+    ]);
+
+    /* TODO aggiungere routes verificate per l'utente
+     * Route::middleware(['auth', 'verified'])->group(function () {
+
+        Route::apiResources([
+            "orders" => \App\Http\Controllers\OrderController::class,
+        ]);
+    });*/
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
