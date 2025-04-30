8dd2eda2198ffb62f25913810acfb93db6fbcb04
Simone Franco <simone.franco@ipsumlab.it>
2025-04-30 17:59:48 +0200

    update route aggiunto name

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Inertia\Inertia;
 
 Route::get("/", function() {
     return Inertia::render('welcome');
-});
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
