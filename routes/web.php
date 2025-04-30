9b7bf3599d11c137acff246bcd4ac2b8358b9c01
Simone Franco <simone.franco@ipsumlab.it>
2025-04-30 17:40:13 +0200

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get("/", function() {
-    return view ('welcome');
+    return Inertia::render('welcome');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
